729f9a7591134116d55970e05287813011e5829d
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2019-12-24 14:39:44 -0600

    Update 02_datatypes.dart
    Conociendo mas sobre las listas, sets y comportamientos

--- ejercicios/01_Basico/02_datatypes.dart
+++ ejercicios/01_Basico/02_datatypes.dart
@@ -30,14 +30,67 @@ String multilinea = '''
 Hola como estás
 ''';
 
-print(nombre3);
-print(multilinea);
+//print(nombre3);
+//print(multilinea);
+
+
+//Boleanos
+bool activo = !false;
+bool falso = false;
+bool corriendo;
+print(corriendo);
+
+if(activo){
+  print('Activo');
+}else{
+  print('Apagado');
+}
+
 
   //listas - Arreglos
+var personajes = ['Superman', 'Batman', 10, true];
+//en dart todo es un objeto
 
+List<String> personajes2 = ['Superman', 'Batman'];
+List<int> enteros = [1, 2, 3, 4];
+
+  print(personajes2);
+    print(enteros);
+
+
+List<String> addes = new List();
+addes.add('Agrego');
+addes.addAll(['numero', 'dodes']);
+addes..add('Batmanm')..add('nueross');
+print(addes);
+
+
+
+List<String> addes2 = new List(3);
+//addes2.addAll(['x','3','9']);
+addes2[0] = 'Lex';
+addes2[1] = 'Lex';
+addes2[2] = 'Lex';
+print(addes2);
 
 
   //Sets
+
+Set<String> Setsx = {'otros', 'noson', 'mejores'};
+print(Setsx);
+Setsx.add('Agrego');
+print(Setsx);
+Setsx.add('Agrego');
+print(Setsx);
+
+
+//los sets no permiten valores duplicados
+
+
+
+
+
+
 
 
 
