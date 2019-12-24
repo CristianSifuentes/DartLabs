c99f53087bb5e5ae00d820709b8f9600993b0600
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2019-12-24 14:16:18 -0600

    Update 02_datatypes.dart
    entendiendo los strings

--- ejercicios/01_Basico/02_datatypes.dart
+++ ejercicios/01_Basico/02_datatypes.dart
@@ -2,29 +2,36 @@ main(List<String> args) {
   
   //numeros
   int a = 10;
-  print(a);
+  //print(a);
 
   double b = 10;
-  print(b);
+  //print(b);
 
   int c;
 
   int x = 10, y = 20, z = 50;
-  print(x);
-  print(y);
-  print(z);
+  //print(x);
+  //print(y);
+  //print(z);
 
-  print(x + y + z);
+  //print(x + y + z);
 
   double $b = 56.23;
 
-  print($b);
+  //print($b);
 
 
 
   //String - Cadena de caracteres
+String nombre = 'Tony';
+String nombre2 = "Tony2"; 
+String nombre3 = '0\´Connor';
+String multilinea = '''
+Hola como estás
+''';
 
-
+print(nombre3);
+print(multilinea);
 
   //listas - Arreglos
 
