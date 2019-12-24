343a95d5a2fe2fa5e3d25aa474ee0b988ef31255
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2019-12-24 14:06:23 -0600

    Create 02_datatypes.dart
    Entendiendo los tipos de datos

---
+++ ejercicios/01_Basico/02_datatypes.dart
@@ -0,0 +1,38 @@
+main(List<String> args) {
+  
+  //numeros
+  int a = 10;
+  print(a);
+
+  double b = 10;
+  print(b);
+
+  int c;
+
+  int x = 10, y = 20, z = 50;
+  print(x);
+  print(y);
+  print(z);
+
+  print(x + y + z);
+
+  double $b = 56.23;
+
+  print($b);
+
+
+
+  //String - Cadena de caracteres
+
+
+
+  //listas - Arreglos
+
+
+
+  //Sets
+
+
+
+  //Mapas
+}
